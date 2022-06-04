b977b8f6f7875b8a1dcd115ccc25f7049615566b
fpunny <frederic.pun@gmail.com>
2022-06-04 00:37:37 -0400

    Finished splash section

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,13 +1,37 @@
-import { StyleProvider } from '@ht6/react-ui';
+import { StyleProvider, Typography } from '@ht6/react-ui';
 import { ReactNode } from 'react';
-import './Page.module.scss';
+import { Toaster, ToastBar } from 'react-hot-toast';
+import { toast } from './Page.module.scss';
 
 export interface PageProps {
   children: ReactNode;
 }
 
 function Page({ children }: PageProps) {
-  return <StyleProvider>{children}</StyleProvider>;
+  return (
+    <StyleProvider>
+      {children}
+      <Toaster
+        position='bottom-center'
+        
+      >
+        {t => (
+          <ToastBar toast={t}>
+            {({ icon, message }) => (
+              <div className={toast}>
+                <span>
+                  {icon}
+                </span>
+                <Typography textType='paragraph2' textWeight={600}>
+                  {message}
+                </Typography>
+              </div>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
+    </StyleProvider>
+  );
 }
 
 export default Page;
